b95188f585012465cc352feadc0689b67cba5491
Patrick dorgu <pat.dorgu@gmail.com>
2020-03-25 10:04:28 -0400

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,59 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/database.js',
+    '/index.html',
+    '/style.css',
+    '/index.js',
+    
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  self.addEventListener('install', function (event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("successfully cached");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  
+  
+    self.clients.claim();
+  ;
+  
+  // fetch
+  self.addEventListener("fetch", function (event) {
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(res => {
+              // If the res was good, clone it and store it in the cache.
+              if (res.status === 200) {
+                cache.put(event.request.url, res.clone());
+              }
+  
+              return res;
+            })
+            .catch(err => {
+              
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+    event.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(event.request).then(res => {
+          return res || fetch(event.request);
+        });
+      })
+    );
+  });
---
+++ routes/apiroutes.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const expenses = require("../models/budget.js");
+
+router.post("/api/budget", ({ body }, res) => {
+  expenses.create(body)
+    .then(dbbudget => {
+      res.json(dbbudget);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+router.get("/api/budget", (req, res) => {
+  expenses.find({})
+    .sort({ date: -1 })
+    .then(dbbudget => {
+      res.json(dbbudget);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+router.post("/api/budget/bulk", ({ body }, res) => {
+    expenses.insertMany(body)
+      .then(dbbudget => {
+        res.json(dbbudget);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+
+module.exports = router;
